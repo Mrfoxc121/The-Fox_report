3e3434074fe0dbea91a542d0c01a8beff86c9f27
Chris Fox <chrisfox@chriss-imac.home>
2022-05-13 22:59:40 +0100

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,35 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookSquare, faTwitterSquare, faInstagramSquare, faPinterestSquare } from '@fortawesome/free-brands-svg-icons'
+import Link from 'next/link'
+
+
+
+
+const Footer = () => {
+
+  return (
+    <div className="text-center my-10">
+      <div className="flex justify-center gap-x-5 mb-4">
+        <Link href="https://www.facebook.com">
+          <FontAwesomeIcon className="text-4xl text-slate-900 cursor-pointer"icon={faFacebookSquare} />
+        </Link>
+        <Link href="https://twitter.com">
+          <FontAwesomeIcon className="text-4xl text-slate-900 cursor-pointer" icon={faTwitterSquare} />
+        </Link>  
+        <Link href="https://instagram.com">
+          <FontAwesomeIcon className="text-4xl text-slate-900 cursor-pointer" icon={faInstagramSquare} />
+        </Link>
+        <Link href="https://pinterest.com">
+          <FontAwesomeIcon className="text-4xl text-slate-900 cursor-pointer" icon={faPinterestSquare} />
+        </Link>
+      </div>
+       
+  
+        <div>
+            <span className="text-xl mb-3">The Fox Report - copyright 2022</span>
+        </div>
+    </div>
+  );
+};
+
+export default Footer;
