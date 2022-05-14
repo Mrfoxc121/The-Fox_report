011b3502e2069f78b02b95e62e753f1f56eba132
Chris Fox <chrisfox@chriss-imac.home>
2022-05-14 10:09:15 +0100

    bg change

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 import { grpahCMSImageLoader } from '../util';
 
 const PostCard = ({ post }) => (
-  <div className="bg-indigo-50 shadow-md rounded-lg p-0 lg:p-8 pb-12 mb-8">
+  <div className="bg-slate-50 shadow-md rounded-lg p-0 lg:p-8 pb-12 mb-8">
     {/* <div className="relative shadow-md inline-block w-full h-60 lg:h-80 mb-6">
       <Image
         unoptimized
